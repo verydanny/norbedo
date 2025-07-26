d6f88b31f3fedbcb42408d4c799becf3a0d3aa34
Daniil Veremchuk <daniel@verydanny.com>
2025-07-26 07:56:38 -0400

    validation

--- src/routes/auth/[type]/+page.server.ts
+++ src/routes/auth/[type]/+page.server.ts
@@ -69,7 +69,7 @@ export const actions = {
     }: RequestEvent): Promise<
         | ActionFailure<{
               error: true
-              errors?: ErrorResponse
+              errors: Partial<ErrorResponse>
           }>
         | undefined
     > => {
